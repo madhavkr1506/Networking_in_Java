52874e869280a3cbf0010c4012da3f1a924f5061
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2024-10-06 12:53:34 +0530

    Add files via upload

--- krishna_.java
+++ krishna_.java
@@ -1,92 +1,94 @@
-import java.net.*;
-import java.io.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.net.*;
+import java.io.*;
 
 public class krishna_ implements ActionListener{
 
-    static JFrame frame = new JFrame("krishna_");
-    static JTextArea chat_area = new JTextArea();
-    static JScrollPane scrollPane = new JScrollPane(chat_area);
-    static JTextField input_field = new JTextField();
-    static JButton send_button = new JButton();
-
-    static Socket client_socket;
-    static BufferedReader reader;
-    static PrintWriter output;
-    static boolean running = true;
-    static String client_name = "Krishna";
-
-    static JButton load_image = new JButton("Img");
-
-    static OutputStream output_stream;
-    static FileInputStream file_input;
-
-
+    JFrame frame = new JFrame("krishna_");
+    JTextArea chat_area = new JTextArea();
+    JScrollPane scrollPane = new JScrollPane(chat_area);
+    JTextField input_field = new JTextField();
+    JButton send_button = new JButton();
+    Socket client_socket;
+    PrintWriter output_writer;
+    BufferedReader reader;
+    boolean running = true;
+    String client_name = "krishna_: ";
+    
     krishna_(){
         prepareGui();
-        handleclient();
-        addActionListener();
+        handle_multiple_client();
+        addActionListeners();
     }
 
-    public static void prepareGui(){
+    void prepareGui(){
         frame.setSize(300,500);
-        frame.getContentPane().setBackground(Color.decode("#dda15e"));
         frame.setLocationRelativeTo(null);
+        frame.getContentPane().setBackground(Color.decode("#40E0D0"));
         frame.getContentPane().setLayout(null);
-
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-
-        chat_area.setBackground(Color.decode("#fefae0"));
-        chat_area.setLineWrap(true);
-        chat_area.setWrapStyleWord(true);
-        chat_area.setEditable(false);
-        scrollPane.setSize(300,400);
-        scrollPane.setLocation(0,0);
         
 
-        input_field.setSize(120,30);
-        input_field.setLocation(15,420);
+        chat_area.setBackground(Color.decode("#000080"));
+        chat_area.setEditable(false);
+        chat_area.setWrapStyleWord(true);
+        chat_area.setLineWrap(true);
+        chat_area.setForeground(Color.decode("#000000"));
+        chat_area.setFont(new Font("Arial",Font.CENTER_BASELINE,15));
+        scrollPane.setSize(300,400);
+        scrollPane.setLocation(0,0);
 
-        load_image.setText("Img");
-        load_image.setSize(60,30);
-        load_image.setLocation(135,420);
-        load_image.setEnabled(false);
+        input_field.setSize(170,30);
+        input_field.setLocation(10,410);
 
         send_button.setText("Send");
         send_button.setSize(70,30);
-        send_button.setLocation(200,420);
+        send_button.setLocation(200,410);
         send_button.setEnabled(false);
 
         frame.getContentPane().add(scrollPane);
         frame.getContentPane().add(input_field);
-        frame.getContentPane().add(load_image);
         frame.getContentPane().add(send_button);
 
-        frame.revalidate();
-        frame.repaint();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 
-    public static void handleclient(){
+    void addActionListeners(){
+        send_button.addActionListener(this);
+    }
+
+    public void actionPerformed(ActionEvent e){
+        Object source = e.getSource();
+        if(source == send_button){
+            String message = input_field.getText().trim();
+            if(output_writer != null){
+                output_writer.println(client_name + message);
+                output_writer.flush();
+                input_field.setText("");
+            }
+        }
+    }
+    public static void main(String[] args) {
+        new krishna_();
+    }
+
+    void handle_multiple_client(){
         try{
             client_socket = new Socket("localhost",50000);
+            output_writer = new PrintWriter(client_socket.getOutputStream());
             reader = new BufferedReader(new InputStreamReader(client_socket.getInputStream()));
-            output = new PrintWriter(client_socket.getOutputStream());
-
             send_button.setEnabled(true);
-            load_image.setEnabled(true);
 
             new Thread(new Runnable() {
                 public void run(){
-                    
+                    String server_message;
                     try{
-                        String serverMessage;
-                        while (running && (serverMessage = reader.readLine()) != null) {
-                            chat_area.append(serverMessage + "\n");
+                        while (running && (server_message = reader.readLine()) != null) {
+                            System.out.println(server_message);
+                            chat_area.append(server_message + "\n");
                         }
-
                     }catch(Exception e){
                         e.printStackTrace();
                     }finally{
@@ -102,69 +104,10 @@ public class krishna_ implements ActionListener{
                 }
             }).start();
 
-
         }catch(Exception e){
             e.printStackTrace();
         }
     }
 
-    public void addActionListener(){
-        send_button.addActionListener(this);
-        load_image.addActionListener(this);
-    }
-
-    public void actionPerformed(ActionEvent e){
-        Object object = e.getSource();
-        if(object == send_button){
-            String message = input_field.getText().trim();
-            input_field.setText("");
-            if(output != null){
-                output.println(client_name + ": " + message);
-                output.flush();
-            }
-            else{
-                System.out.println("Connection is not established yet, please try after some time.");
-            }
-        }
-        if(object == load_image){
-            JFileChooser fileChooser = new JFileChooser();
-            fileChooser.setDialogTitle("Select Img");
-            int result = fileChooser.showOpenDialog(null);
-            if(result == JFileChooser.APPROVE_OPTION){
-                File image  = fileChooser.getSelectedFile();
-                try{
-                    if(image != null){
-                        sendImage(image);
-                    }
-                }catch(Exception ex){
-                    ex.printStackTrace();
-                }
-            }
-        }
-    }
-
-    public static void sendImage(File image_path){
-        try{
-            output_stream = client_socket.getOutputStream();
-            file_input = new FileInputStream(image_path);
-
-            byte[] buffer = new byte[4096];
-            int byteRead;
-
-            while ((byteRead = file_input.read(buffer)) > 0) {
-                output_stream.write(buffer, 0, byteRead);
-                output_stream.flush();
-            }
-
-            file_input.close();
-
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    }
-
-
-    public static void main(String[] args) {
-        new krishna_();
-    }
+    
 }
--- server_.java
+++ server_.java
@@ -1,23 +1,23 @@
-import java.net.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.*;
-
 
 public class server_ {
 
-    private static List<PrintWriter> client_writer_list = new ArrayList<>();
+    static List<PrintWriter> client_writer_list = new ArrayList<>();
     public static void main(String[] args) {
-
-
-        try(ServerSocket server_socket = new ServerSocket(50000)){
+        try(ServerSocket serverSocket = new ServerSocket(50000)){
             System.out.println("Server is ready to listen on port 50000");
 
             while (true) {
-                Socket client_socket = server_socket.accept();
+                Socket client_socket = serverSocket.accept();
                 new handle_multiple_client(client_socket).start();
-            }
-            
 
+            }
 
         }catch(Exception e){
             e.printStackTrace();
@@ -26,30 +26,34 @@ public class server_ {
 
     static class handle_multiple_client extends Thread{
         private Socket client_socket;
-        private PrintWriter output;
-        public handle_multiple_client(Socket client_socket){
+        private PrintWriter output_writer;
+        handle_multiple_client(Socket client_socket){
             this.client_socket = client_socket;
         }
 
         public void run(){
             try{
                 BufferedReader reader = new BufferedReader(new InputStreamReader(client_socket.getInputStream()));
-                output = new PrintWriter(client_socket.getOutputStream());
 
+                output_writer = new PrintWriter(client_socket.getOutputStream());
                 synchronized(client_writer_list){
-                    client_writer_list.add(output);
+                    client_writer_list.add(output_writer);
                 }
-                String inputline;
-                while ((inputline = reader.readLine()) != null) {
-                    System.out.println("Received: " + inputline);
-                    broadCasting(inputline);
+
+                String inputLine;
+                while ((inputLine = reader.readLine()) != null) {
+                    System.out.println("Received: " + inputLine);
+
+                    broad_casting(inputLine);
+
                 }
+
 
             }catch(Exception e){
                 e.printStackTrace();
             }finally{
                 synchronized(client_writer_list){
-                    client_writer_list.remove(output);
+                    client_writer_list.remove(output_writer);
                 }
                 try{
                     client_socket.close();
@@ -58,15 +62,14 @@ public class server_ {
                 }
             }
         }
-
-
+        
     }
 
-    private static void broadCasting(String message){
+    static void broad_casting(String message){
         synchronized(client_writer_list){
-            for(PrintWriter writer : client_writer_list){
-                writer.println(message);
-                writer.flush();
+            for(PrintWriter output_writer : client_writer_list){
+                output_writer.println(message);
+                output_writer.flush();
             }
         }
     }
